cf4444f00427bb09b0abbc6ca9e6f0ce16ed64d0
Yulia Sheynkman <Yulias@Kayhut.local>
2020-11-24 03:27:51 -0800

    part 2 CR_3

--- server.py
+++ server.py
@@ -7,16 +7,21 @@ def main():
     serv.listen(5)
     while True:
         conn, addr = serv.accept()
-        print 'Client from address: {} conn: {}'.format(addr, conn)
+        print 'Client from address: {} '.format(addr)
         data = "Start"
         while data != "":
-            data = conn.recv(4096)
+            try:
+                data = conn.recv(4096)
+            except:
+                print "Client aborted"
+                break
             if data == "SHUTDOWN":
                 conn.sendall("GOODBYE")
                 break
             else:
                 print data
                 conn.send(data + data)
+
         conn.close()
         print conn
         print 'client disconnected'
