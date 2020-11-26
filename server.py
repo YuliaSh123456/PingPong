1b50533ceab4d5632e34085cff8236d1cbfb1270
Yulia Sheynkman <Yulias@Kayhut.local>
2020-11-26 01:10:24 -0800

    part 3 CR_1

--- server.py
+++ server.py
@@ -1,28 +1,84 @@
-import socket
+import select, socket, sys, Queue
+
+inputs = []
+outputs = []
+message_queues = {}
+
+
+def add_new_client(new_socket):
+    connection, client_address = new_socket.accept()
+    connection.setblocking(0)
+    inputs.append(connection)
+    message_queues[connection] = Queue.Queue()
+
+
+def handle_client_input(socket_to_read):
+    try:
+        data = socket_to_read.recv(1024)
+    except socket.error, e:
+        print "Client aborted " + str(e)
+
+        if socket_to_read in outputs:
+            outputs.remove(socket_to_read)
+
+        inputs.remove(socket_to_read)
+        socket_to_read.close()
+        del message_queues[socket_to_read]
+        return False
+
+    if data:
+        message_queues[socket_to_read].put(data + data)
+        if socket_to_read not in outputs:
+            outputs.append(socket_to_read)
+    else:
+        if socket_to_read in outputs:
+            outputs.remove(socket_to_read)
+        inputs.remove(socket_to_read)
+        socket_to_read.close()
+        del message_queues[socket_to_read]
+
+    return True
+
+
+def handle_client_outut(socket_to_write):
+    try:
+        next_msg = message_queues[socket_to_write].get_nowait()
+    except Queue.Empty:
+        outputs.remove(socket_to_write)
+    else:
+        try:
+            socket_to_write.send(next_msg)
+        except socket.error, e:
+            print "Client aborted " + str(e)
 
 
 def main():
-    serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serv.bind(('localhost', 50000))
-    serv.listen(5)
-    while True:
-        conn, addr = serv.accept()
-        print 'Client from address: {} '.format(addr)
-        data = "Start"
-        while data != "":
-            try:
-                data = conn.recv(4096)
-            except socket.error, e:
-                print "Client aborted "+str(e)
-                break
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setblocking(0)
+    server.bind(('localhost', 50000))
+    server.listen(5)
 
-            if data != "":
-                print data
-                conn.send(data + data)
+    inputs.append(server)
 
-        conn.close()
-        print 'client disconnected'
-    serv.close()
+    while inputs:
+        readable, writable, exceptional = select.select(inputs, outputs, inputs)
+
+        for s in readable:
+            if s is server:
+                add_new_client(s)
+            else:
+                if not handle_client_input(s):
+                    continue
+
+        for s in writable:
+            handle_client_outut(s)
+
+        for s in exceptional:
+            inputs.remove(s)
+            if s in outputs:
+                outputs.remove(s)
+            s.close()
+            del message_queues[s]
 
 
 if __name__ == "__main__":
